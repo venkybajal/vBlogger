9b983aff8897e8856183f16a1ed8c2e6adf55536
=Venkatesh <=venkybajal@gmail.com>
2014-05-22 19:02:42 +0530

    Delete and Form added

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -5,3 +5,4 @@
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
+ActionController::API.send :include, ActionController::StrongParameters
